39daa2b8598674c09e437179603fc77316adc8d7
Rikesh Lal Shrestha <rikeshlakhe@gmail.com>
2021-10-11 15:51:09 +0545

    Jira Issue fixed

--- src/routes/v4/issues.js
+++ src/routes/v4/issues.js
@@ -39,112 +39,117 @@ router.post("/", async function (req, res) {
   let issues = [];
   let i = 0;
   let chunk_size = 100;
-
+  let chunkData = undefined
   while (true) {
-    let chunkData = await jira
+    chunkData = await jira
       .searchJira(
         `resolved >= ${startDate} AND resolved <= ${endDate} AND project = ${projectKey} AND resolution = Done ORDER BY cf[10004] DESC, created DESC`,
         {
           startAt: i,
           maxResults: chunk_size,
+          fields: ["customfield_10004","customfield_12315","customfield_10007"]
         }
       )
       .catch((err) => {
         res.status(500).send(err);
       });
 
-    chunkData.issues.forEach((issue) => {
-      if (issue.fields.customfield_10004) {
-        totalSP += issue.fields.customfield_10004;
-
-        if (
-          issue.fields.customfield_12315 &&
-          issue.fields.customfield_12315.length > 0
-        ) {
-          issue.fields.customfield_12315.map((scope) => {
-            // Scope change calculation id:10931
-            if (scope.id === "10931") {
-              totalSChange += issue.fields.customfield_10004;
-            }
-            // Scope creep calculation id:10932
-            if (scope.id === "10932") {
-              totalSCreep += issue.fields.customfield_10004;
-            }
-          });
-        }
-
-
-        if (
-          issue.fields.customfield_10007 &&
-          issue.fields.customfield_10007.length > 0
-        ) {
-          let tempSprintData = issue.fields.customfield_10007[0];
-  
-          if(tempSprintData.id in sprint){
-            sprint[tempSprintData.id].issues = [...sprint[tempSprintData.id]?.issues, issue];
-            sprint[tempSprintData.id].totalStoryPoint = sprint[tempSprintData.id].totalStoryPoint + issue.fields.customfield_10004;
-  
-            if (
-              issue.fields.customfield_12315 &&
-              issue.fields.customfield_12315.length > 0
-            ) {
-              issue.fields.customfield_12315.map((scope) => {
-                // Scope change calculation id:10931
-                if (scope.id === "10931") {
-                  sprint[tempSprintData.id].scopeChange = issue.fields.customfield_10004 + (sprint[tempSprintData.id].scopeChange || 0);
-                }
-                // Scope creep calculation id:10932
-                if (scope.id === "10932") {
-                  sprint[tempSprintData.id].scopeCreep = issue.fields.customfield_10004 + (sprint[tempSprintData.id].scopeCreep || 0);
-                }
-              });
-            }
-          }else{
-            sprint[tempSprintData.id] = {
-              ...tempSprintData,
-              issues: [issue],
-              scopeChange: 0,
-              scopeCreep: 0,
-              totalStoryPoint: (issue.fields.customfield_10004 || 0),
-            };
-  
-            if (
-              issue.fields.customfield_12315 &&
-              issue.fields.customfield_12315.length > 0
-            ) {
-              issue.fields.customfield_12315.map((scope) => {
-                // Scope change calculation id:10931
-                if (scope.id === "10931") {
-                  sprint[tempSprintData.id].scopeChange = issue.fields.customfield_10004;
-                }
-                // Scope creep calculation id:10932
-                if (scope.id === "10932") {
-                  sprint[tempSprintData.id].scopeCreep = issue.fields.customfield_10004;
-                }
-              });
-            }
-          
-          }
-        }
-      }
-
-      
-    });
-
     i += 1;
     issues = issues.concat(chunkData.issues);
-    chunk_size = chunkData.total - chunk_size;
+    chunk_size = Math.abs(chunkData.total - chunk_size);
+
+    
     if (issues.length >= chunkData.total) {
       break;
     }
   }
+
+  issues.forEach((issue,index) => {
+    console.log("issue",issue.fields);
+    if (issue.fields.customfield_10004) {
+
+      totalSP += issue.fields.customfield_10004;
+
+      if (
+        issue.fields.customfield_12315 &&
+        issue.fields.customfield_12315.length > 0
+      ) {
+        issue.fields.customfield_12315.map((scope) => {
+          // Scope change calculation id:10931
+          if (scope.id === "10931") {
+            totalSChange += issue.fields.customfield_10004;
+          }
+          // Scope creep calculation id:10932
+          if (scope.id === "10932") {
+            totalSCreep += issue.fields.customfield_10004;
+          }
+        });
+      }
+
+
+      if (
+        issue.fields.customfield_10007 &&
+        issue.fields.customfield_10007.length > 0
+      ) {
+        let tempSprintData = issue.fields.customfield_10007[0];
+
+        if(tempSprintData.id in sprint){
+          sprint[tempSprintData.id].issues = [...sprint[tempSprintData.id]?.issues, issue];
+          sprint[tempSprintData.id].totalStoryPoint = sprint[tempSprintData.id].totalStoryPoint + issue.fields.customfield_10004;
+
+          if (
+            issue.fields.customfield_12315 &&
+            issue.fields.customfield_12315.length > 0
+          ) {
+            issue.fields.customfield_12315.map((scope) => {
+              // Scope change calculation id:10931
+              if (scope.id === "10931") {
+                sprint[tempSprintData.id].scopeChange = issue.fields.customfield_10004 + (sprint[tempSprintData.id].scopeChange || 0);
+              }
+              // Scope creep calculation id:10932
+              if (scope.id === "10932") {
+                sprint[tempSprintData.id].scopeCreep = issue.fields.customfield_10004 + (sprint[tempSprintData.id].scopeCreep || 0);
+              }
+            });
+          }
+        }else{
+          sprint[tempSprintData.id] = {
+            ...tempSprintData,
+            issues: [issue],
+            scopeChange: 0,
+            scopeCreep: 0,
+            totalStoryPoint: (issue.fields.customfield_10004 || 0),
+          };
+
+          if (
+            issue.fields.customfield_12315 &&
+            issue.fields.customfield_12315.length > 0
+          ) {
+            issue.fields.customfield_12315.map((scope) => {
+              // Scope change calculation id:10931
+              if (scope.id === "10931") {
+                sprint[tempSprintData.id].scopeChange = issue.fields.customfield_10004;
+              }
+              // Scope creep calculation id:10932
+              if (scope.id === "10932") {
+                sprint[tempSprintData.id].scopeCreep = issue.fields.customfield_10004;
+              }
+            });
+          }
+        
+        }
+      }
+    }
+  });
+
+  console.log('here',chunk_size, chunkData.total,issues.length,sprint);
   res.status(200).send({
     total: issues.length,
     totalSP,
     totalSChange,
     totalSCreep,
     sprint,
-    // issues,
+    issues,
   });
 });
 
