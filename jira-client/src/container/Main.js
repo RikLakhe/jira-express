2ca1409df2bdeba590593b414a30192d6445e425
Rikesh Lal Shrestha <rikeshlakhe@gmail.com>
2021-10-19 22:28:47 +0545

    Working with /sprint route and add sprint component

--- jira-client/src/container/Main.js
+++ jira-client/src/container/Main.js
@@ -9,7 +9,15 @@ import {
   InputLabel,
   FormHelperText,
   TextField,
+  Grid,
+  Card,
+  Accordion,
+  AccordionSummary,
+  Typography,
+  AccordionDetails,
 } from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
 import { Line } from "react-chartjs-2";
 
 import { API_URL } from "../constant/appConfig";
@@ -31,6 +39,8 @@ const Main = (props) => {
   const [loading, setLoading] = React.useState(false);
 
   const [lineData, setLineData] = React.useState({});
+
+  const [sprintList, setSprintList] = React.useState([]);
 
   const handleProjectSelect = (value) => {
     setSelectedProject(value);
@@ -109,31 +119,17 @@ const Main = (props) => {
               };
             }
 
+            let tempSprintList = [];
+
             Object.entries(res.data.sprint).forEach(async ([key, value]) => {
               tempLineData.labels.push(value.name);
               tempLineData.datasets[0].data.push(value.totalStoryPoint);
               tempLineData.datasets[1].data.push(value.scopeChange);
               tempLineData.datasets[2].data.push(value.scopeCreep);
-
-              console.log('index',key)
-              await axios
-                .post(API_URL + "/v4/sprints", {
-                  boardId: value.boardId,
-                  spintId: value.id,
-                })
-                .then((res) => {
-                  if (res && res.status === 200) {
-                    console.log('value.id',value.id)
-                    console.log("res.data",res.data)
-                  }
-                })
-                .catch((err) => {
-                  console.log(err);
-                });
-
-              console.log('index end',key)
-
+              tempSprintList.push(value);
             });
+
+            setSprintList(tempSprintList);
             setLineData(tempLineData);
           }
 
@@ -220,25 +216,128 @@ const Main = (props) => {
         </Button>
       )}
 
-      {loading ? (
-        <h3>Loading....</h3>
-      ) : result ? (
-        <p>
-          <h3>Total SP:{result.totalSP}</h3>
-          <br />
-          <h3>Total Scope Change:{result.totalSChange}</h3>
-          <br />
-          <h3>Total Scope Creep:{result.totalSCreep}</h3>
-          <br />
-          {lineData && (
-            <div className="chart-div">
-              <Line data={lineData} />
-            </div>
-          )}
-        </p>
-      ) : (
-        <h3>No jira issues</h3>
-      )}
+      <Grid container spacing={3}>
+        {loading ? (
+          <Grid item xs={12}>
+            <h3>Loading....</h3>
+          </Grid>
+        ) : result ? (
+          <>
+            <Grid item xs={6}>
+              <h3>Total SP:{result.totalSP}</h3>
+              <h3>Total Scope Change:{result.totalSChange}</h3>
+              <h3>Total Scope Creep:{result.totalSCreep}</h3>
+              <Card variant="outlined">
+                {lineData && (
+                  <div className="chart-div">
+                    <Line data={lineData} />
+                  </div>
+                )}
+              </Card>
+            </Grid>
+            <Grid item xs={6}>
+              <SprintReport sprintList={sprintList} />
+            </Grid>
+          </>
+        ) : (
+          <Grid item xs={12}>
+            <h3>No jira issues</h3>
+          </Grid>
+        )}
+      </Grid>
+    </>
+  );
+};
+
+const SprintReport = (props) => {
+  const [expanded, setExpanded] = React.useState(false);
+  const [sprintLoading, setSprintLoading] = React.useState(false);
+  const [sprintData, setSprintData] = React.useState(undefined);
+  const { sprintList } = props;
+
+  if (sprintList && sprintList.length < 0) {
+    return <Card variant="outlined">No sprint data</Card>;
+  }
+
+  const handleSprintSelect = (sprint) => async (event, isExpanded) => {
+    setSprintLoading(true);
+    setSprintData(undefined);
+    setExpanded(sprint.name);
+
+    await axios
+      .post(API_URL + "/v4/sprints", {
+        boardId: sprint.boardId,
+        spintId: sprint.id,
+      })
+      .then((res) => {
+        if (res && res.status === 200) {
+          console.log("here", res.data);
+          setSprintData(res.data);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    setSprintLoading(false);
+  };
+
+  return (
+    <>
+      {sprintList.map((sprint, index) => (
+        <Accordion
+          expanded={expanded === sprint.name}
+          onChange={handleSprintSelect(sprint)}
+        >
+          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography variant="h6">{sprint.name}</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            {sprintLoading ? (
+              <h3>Loading....</h3>
+            ) : sprintData ? (
+              <div>
+                <Typography>
+                  <strong>Start Date: </strong>
+                  {moment(sprintData.sprint.isoStartDate).format("YYYY/MM/DD")}
+                </Typography>
+                {sprintData.sprint.completeDate !== "None" && (
+                  <Typography>
+                    <strong>Complete Date: </strong>
+                    {moment(sprintData.sprint.completeDate).format(
+                      "YYYY/MM/DD"
+                    )}
+                  </Typography>
+                )}
+                <Typography style={{ whiteSpace: "pre-line" }}>
+                  <strong>Goals</strong>
+                  <br />
+                  {sprintData.sprint.goal}
+                </Typography>
+
+                <Typography>
+                  <strong>Inital Estimation: </strong>
+                  {sprintData.contents.completedIssuesEstimateSum.value+sprintData.contents.issuesNotCompletedEstimateSum.value}
+                </Typography>
+
+                <Typography>
+                  <strong>Completed: </strong>
+                  {sprintData.contents.completedIssuesEstimateSum.value}<br />
+                  {sprintData.contents.completedIssues.map((issue) => (issue.key+" "))}
+                </Typography>
+
+                <Typography>
+                  <strong>Remaining: </strong>
+                  {sprintData.contents.issuesNotCompletedEstimateSum.value}<br />
+                  {sprintData.contents.issuesNotCompletedInCurrentSprint.map((issue) => (issue.key+" "))}
+                </Typography>
+              </div>
+            ) : (
+              <h3>No sprint data</h3>
+            )}
+          </AccordionDetails>
+        </Accordion>
+      ))}
     </>
   );
 };
